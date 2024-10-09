35b16ca1f1fbe8c79e0788d25e1cbe0a7407548e
Rohitdhall987 <rohitdhall987@gmail.com>
2024-10-09 15:21:54 +0530

    first commit

---
+++ android/app/src/main/kotlin/in/ridewave/ridewave/MainActivity.kt
@@ -0,0 +1,5 @@
+package `in`.ridewave.ridewave
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/app.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:ridewave/screens/SplashScreen.dart';
+import 'package:ridewave/utils/AppColors.dart';
+
+
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return ProviderScope(
+      child: MaterialApp.router(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: AppColors.blue),
+          textTheme: GoogleFonts.robotoTextTheme(),
+          useMaterial3: true,
+        ),
+        routerConfig: _routes,
+      ),
+    );
+  }
+  final _routes = GoRouter(
+      routes: [
+        GoRoute(
+            path: "/",
+            name: "splash",
+            builder: (state,context)=>const Splashscreen()
+        ),
+        GoRoute(
+            path: "/home",
+            name: "home",
+            builder: (state,context)=>const Placeholder()
+        ),
+        GoRoute(
+            path: "/login",
+            name: "login",
+            builder: (state,context)=>const Placeholder()
+        ),
+      ]
+  );
+}
